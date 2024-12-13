200a0003f60afe4fb9cbef66ef4a7ce3b6f08102
xinhuatan <xinhuatan@tencent.com>
2024-12-13 21:25:26 +0800

    add toolwindowmanager

--- changhe_studio.cpp
+++ changhe_studio.cpp
@@ -1,9 +1,12 @@
 #include "changhe_studio.h"
 
+#include "ToolWindowManager.h"
+
 changhe_studio::changhe_studio(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    ui.tool_window_manager->addToolWindow(new QWidget(this), ToolWindowManager::LastUsedArea);
 }
 
 changhe_studio::~changhe_studio()
